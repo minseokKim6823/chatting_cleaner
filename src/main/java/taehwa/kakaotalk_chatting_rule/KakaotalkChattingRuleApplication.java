dec62ec474f55aadb5d3173e1997d7acde981e00
minseok <minseokkim6823@taehwainno.com>
2026-03-18 17:20:33 +0900

    initial commit for chatting_cleaner

---
+++ src/main/java/taehwa/kakaotalk_chatting_rule/KakaotalkChattingRuleApplication.java
@@ -0,0 +1,13 @@
+package taehwa.kakaotalk_chatting_rule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaotalkChattingRuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KakaotalkChattingRuleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/taehwa/kakaotalk_chatting_rule/KakaotalkChattingRuleApplicationTests.java
@@ -0,0 +1,13 @@
+package taehwa.kakaotalk_chatting_rule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaotalkChattingRuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
